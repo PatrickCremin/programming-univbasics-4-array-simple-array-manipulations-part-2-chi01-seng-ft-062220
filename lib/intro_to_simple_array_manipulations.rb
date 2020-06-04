28cee62158ee82b44878e363261a503dd9f2885c
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-04 02:52:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+arr1 = ["raindrops on roses", "whiskers on kittens"]
+arr2 = ["sports cars", "flatiron school"]
+
+def using_concat (arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr1, element)
+  arr1.insert(4, element)
+end
+
+def using_uniq(arr1)
+  arr1.uniq
+end
+
+a = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+
+def using_flatten(c)
+  c.flatten
+end
+
+def using_delete(arr, string)
+  arr.delete(string)
+end
+
+
+def using_delete_at (arr3, i)
+  arr3 = ["Johnny 5", "R2D2", "Robocop"]
+  i = 2
+  newarr = arr3.delete_at(i)
+end
